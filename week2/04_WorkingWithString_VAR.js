80848e747279f20c56498342926eee8156938d0d
jamsrahimi <rahimi@protonmail.com>
2017-12-27 19:53:57 +0100

    upload 2

---
+++ week2/04_WorkingWithString_VAR.js
@@ -0,0 +1,8 @@
+'use strict' //this is to define clear variables and prevent poor coding
+
+let y = "I procrastinate!"; //I tried assigning it with const but it failed on later assignment
+console.log("The value of my string will be: \'I procrastinate!\'");
+console.log(y);
+y = "Ik stel mij uit!";
+console.log("The new assigned value of my string is: \'Ik stel mij uit!\'");
+console.log(y);

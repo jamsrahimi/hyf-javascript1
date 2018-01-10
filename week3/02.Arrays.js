308402997aab957d00172e2a5adbd877af452813
jamsrahimi <rahimi@protonmail.com>
2018-01-11 00:38:35 +0100

    week3 2nd commit

--- week3/02.Arrays.js
+++ week3/02.Arrays.js
@@ -6,12 +6,12 @@ console.log(favoriteAnimals);
 
 favoriteAnimals.splice(1, 0, "meerkat");
 console.log("\nThe new array is: blowfish,meerkat,capricorn,giraffe,turtle");
-console.log(favoriteAnimals, "\n");
+console.log(favoriteAnimals + "\n");
 
 console.log("The array has a length of: ", favoriteAnimals.length);
 
 favoriteAnimals.splice(3, 3, "turtle");
-console.log(favoriteAnimals, "\n");
+console.log(favoriteAnimals + "\n");
 
 let found = favoriteAnimals.indexOf("meerkat");
 console.log("\nThe item you are looking for is at index: ", found);
